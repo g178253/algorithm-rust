3eadaaeba6f7b39219cddaecfb83e167c94622d0
haha <gxd2013@outlook.com>
2019-02-23 09:11:52 +0800

    重构插入排序

--- src/sort/insertion.rs
+++ src/sort/insertion.rs
@@ -4,10 +4,10 @@ pub fn sort(a: &mut Vec<i32>) {
     if a.len() < 2 { return; }
 
     for i in 1..a.len() {
-        let mut j = i;
-        while j > 0 && a[j] < a[j-1] {
-            common::swap(a, j, j-1);
-            j -= 1;
+        for j in (1..=i).rev() {
+            if a[j] < a[j-1] {
+                common::swap(a, j, j-1);
+            }
         }
     }
 }
